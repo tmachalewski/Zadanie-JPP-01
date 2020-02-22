76ea2a1a0a2e9e480a0b0ab67c7e30243ec193a9
tommach <tomek.machalewski@gmail.com>
2020-02-22 15:44:47 +0100

    added main funcionality

--- src/pl/sda/Main.java
+++ src/pl/sda/Main.java
@@ -1,7 +1,18 @@
 package pl.sda;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = sc.nextLine();
+        System.out.println("What is your surname?");
+        String surname = sc.nextLine();
+        System.out.println("What is your name?");
+        int age = sc.nextInt();
+
+        System.out.println("Name "+name+" Surname: " +surname + " Age: "+age);
+
     }
 }
